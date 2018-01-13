5c4211d6631ad812778d96f077a8d1e8fd4d5193
mori <997462392@qq.com>
2018-01-14 01:18:01 +0800

    proxy

--- webpack-config/devServer.js
+++ webpack-config/devServer.js
@@ -1,16 +1,25 @@
-const path = require('path');
-const dirPath = require('./base/dir-path');
-const os = require('os');
+const path = require("path");
+const dirPath = require("./base/dir-path");
+const os = require("os");
 const host = os.networkInterfaces()["WLAN 2"][1].address;
 
 module.exports = {
-    proxy: { // proxy URLs to backend development server
-        '/api': 'http://localhost:3000'
+    proxy: {
+        // proxy URLs to backend development server
+        "/api": "http://localhost:3000"
     },
     open: true,
     port: 9000,
     contentBase: dirPath.dist, // boolean | string | array, static file location
     compress: true, // enable gzip compression
     hot: true,
-    host: host
-}
+    host: host,
+    proxy: {
+        "**": {
+            target: "http://www.xxx.com",
+            secure: false,
+            changeOrigin: true,
+            pathRewrite: { "^/api": "" }
+        }
+    }
+};
